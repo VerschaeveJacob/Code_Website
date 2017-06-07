f4b9db72f937277fa6865e94dd2adaedb1916e52
jacob <jacob.verschaeve@student.howest.be>
2017-06-07 08:52:10 +0200

    Dinsdag 6 Juni / SQL verwerken in website

--- Air_Check.py
+++ Air_Check.py
@@ -1,12 +1,13 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request
 import os
+from dbClass import dbClass
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', methods=["GET","POST"])
 def index():
-    return render_template('grafiek_CO2.html')
+    return render_template('index.html')
 
 @app.route('/registreer')
 def registreer():
@@ -32,6 +33,53 @@ def dashboard_CO2():
 def grafiek_CO2():
     return render_template('grafiek_CO2.html')
 
+@app.route('/sentContact', methods=["POST"])
+def sentContact():
+    db = dbClass()
+    naam = request.form['naam']
+    print(naam)
+    email = request.form['email']
+    print(email)
+    comment = request.form['commentaar']
+    print(comment)
+    db.insert_comment(naam,email,comment)
+
+    return render_template('contact_verzenden.html')
+
+@app.route('/sentRegistreer', methods=["POST"])
+def sentRegistreer():
+    db = dbClass()
+    email = request.form['email']
+    print(email)
+    password = request.form['password']
+    print(password)
+    password_bevestigen = request.form['password_bevestigen']
+    print(password_bevestigen)
+    serienummer = request.form['serienummer']
+    print(serienummer)
+    db.registreren(email,password,serienummer)
+    print("Gelukt")
+    return render_template('index.html')
+
+@app.route('/contactverzenden')
+def contactverzenden():
+    return render_template("contact_verzenden.html")
+
+@app.route('/inloggen_controleren' , methods=["POST"])
+def inloggen_controleren():
+    db = dbClass()
+    email = request.form['email']
+    wachtwoord = request.form['password']
+
+    aantal = db.inloggen_controleren(email, wachtwoord)
+    print(aantal)
+    print("DB_Controleren")
+    if aantal[0] == 1:
+        print("JA")
+        return render_template("dashboard_CO2.html")
+    else:
+        print("Nee")
+        return render_template("inloggen_verkeerd.html")
 @app.context_processor
 def override_url_for():
     return dict(url_for=dated_url_for)
--- dbClass.py
+++ dbClass.py
@@ -2,7 +2,27 @@ class dbClass():
     def __init__(self):
         import mysql.connector as connector
 
-        self.__dsn = {"host": "localhost", "user": "jacob", "passwd": "root", "db": "db_products"}
+        self.__dsn = {"host": "localhost", "user": "jacob", "passwd": "root", "db": "Air_Check"}
         self.__connection = connector.connect(**self.__dsn)
         self.__cursor = self.__connection.cursor()
 
+    def insert_comment(self, name, email, comment):
+        q = "INSERT INTO tblContact(Naam,Emailadres,Bericht, Tijdstip)" \
+            "VALUES('" + name + "','" + email + "','" + comment + "', now())"
+        self.__cursor.execute(q)
+        self.__connection.commit()
+        self.__connection.close()
+
+    def registreren(self, email, passwoord, serienummer):
+        q = "INSERT INTO tblGegevens(Emailadres,Wachtwoord,Ingelogd_Blijven, Serienummer)" \
+            "VALUES('" + email + "','" + passwoord + "', 0 ,'" + serienummer + "')"
+        self.__cursor.execute(q)
+        self.__connection.commit()
+        self.__connection.close()
+
+    def inloggen_controleren(self, mail, passwoord):
+        q = "SELECT COUNT(*) FROM tblGegevens WHERE Emailadres = '" + mail + "' AND Wachtwoord = '" + passwoord + "';"
+        self.__cursor.execute(q)
+        result = self.__cursor.fetchone()
+        self.__connection.close()
+        return result
